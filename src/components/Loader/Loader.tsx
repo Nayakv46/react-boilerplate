c8d3be6210b5e6bea7bd01f26eeff5890e0b2f91
Nayakv46 <bartoszjaniak04@gmail.com>
2026-06-28 20:23:59 +0200

    Loader component

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,12 @@
+import { Spinner } from "@components/ui/shadcn/spinner";
+
+const Loader = () => {
+  return (
+    <div className="flex items-center gap-2">
+      <Spinner />
+      <p>Loading...</p>
+    </div>
+  );
+};
+
+export default Loader;
