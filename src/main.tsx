7c85da27366b9b6c4abe4a40651cd515c33b9a21
Nayakv46 <bartoszjaniak04@gmail.com>
2026-06-28 20:53:21 +0200

    Styles update

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "./styles/globals.css";
 import App from "./app/App";
 import { AppProviders } from "./app/providers";
 import { Toaster } from "./components/ui/shadcn/sonner";
