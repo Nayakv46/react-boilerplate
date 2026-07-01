8507f980f37d9c3370fac4ec051755831127d181
Nayakv46 <bartoszjaniak04@gmail.com>
2026-07-01 19:02:44 +0200

    Base home link pages

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,6 +4,10 @@ import { ThemeProvider } from "@/components/ui/shadcn/theme-provider";
 import Login from "@/pages/auth/Login";
 import SignUp from "@/pages/auth/SignUp";
 import Navbar from "@/components/Navbar/Navbar";
+import Components from "@/pages/Components/Components";
+import Form from "@/pages/Form/Form";
+import Charts from "@/pages/Charts/Charts";
+import Tables from "@/pages/Tables/Tables";
 
 const App = () => {
   return (
@@ -14,10 +18,10 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
-          <Route path="/components" element={<Home />} />
-          <Route path="/form" element={<Home />} />
-          <Route path="/charts" element={<Home />} />
-          <Route path="/tables" element={<Home />} />
+          <Route path="/components" element={<Components />} />
+          <Route path="/form" element={<Form />} />
+          <Route path="/charts" element={<Charts />} />
+          <Route path="/tables" element={<Tables />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
